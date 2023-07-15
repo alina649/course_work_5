621f5abdce75d50f38e4b395ded6275c254fc5f1
Alina <alinatrofimova649@gmail.com>
2023-07-15 14:06:04 +0800

    2

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,8 @@ def get_hhRU_data(company_ids: list[str]):
     """Получение данных с сайта HH_ru"""
     data = []
     for company_id in company_ids:
-        url_company = f'https://irkutsk.hh.ru/employers/{company_id}'
-
-        url_vacancy = 'https://api.hh.ru/vacancies'
+        url_company = f'https://api.hh.ru/employers/{company_id}' #Информация о названии компании и количество вакансий
+        url_vacancy = 'https://api.hh.ru/vacancies' # нформация о вакансиях
         params = {'employer_id': company_id,
                   'per_page': '10'}
         headers = {
@@ -21,10 +20,10 @@ def get_hhRU_data(company_ids: list[str]):
         response_vacancy = requests.get(url_vacancy, params=params, headers=headers)
         response_company = requests.get(url_company, headers=headers)
 
-        if response_vacancy.status_code == 200:
-            dat_v = response_vacancy.json()
-            dat_c = response_company.json()
-            data.append({'company': dat_c,'vacancy': dat_v['items']})
+        if response_vacancy.status_code == 200 and response_company.status_code == 200:
+            data_vacancy = response_vacancy.json()
+            dat_company = response_company.json()
+            data.append({'company': dat_company, 'vacancy': data_vacancy['items']})
     return data
 
 
@@ -72,35 +71,40 @@ def save_data_to_database(data: list[dict[str, Any]], database_name: str, params
     conn = psycopg2.connect(dbname=database_name, **params)
 
     with conn.cursor() as cur:
+        for company in data:
+            company_id = company['company']['id']
+            title_company = company['company']['name']
+            number_vacancies = company['company']['open_vacancies']
+            cur.execute(
+                """
+                INSERT INTO  company (company_id, title_company, number_vacancies)
+                VALUES (%s, %s, %s)
+                """,
+                (company_id, title_company, number_vacancies)
+            )
+            for vacancy in company['vacancy']:
+                vacancy_name = vacancy['name']
 
-        for companys in data:
-            number_vacancies = companys['found']
-            for company in companys['items']:
-                company_id = company['employer']['id']
-                title_company = company['employer']['name']
-
-                cur.execute(
-                    """
-                    INSERT INTO  company (company_id, title_company, number_vacancies)
-                    VALUES (%s, %s, %s)
-                    """,
-                    (company_id, title_company, number_vacancies)
-                )
-
-                vacancy_name = company['name']
-                if company['salary'] is not None:
-                    salary_from = company['salary']['from']
-                    salary_to = company['salary']['to']
+                if vacancy['salary'] is not None:
+                    if vacancy['salary']['from'] is not None:
+                        salary_from = vacancy['salary']['from']
+                    else:
+                        salary_from = 0
+                    if vacancy['salary']['to'] is not None:
+                        salary_to = vacancy['salary']['to']
+                    else:
+                        salary_to = 0
                 else:
                     salary_from = 0
                     salary_to = 0
-                alternate_url = company['alternate_url']
+
+                url_vacancy = vacancy['alternate_url']
                 cur.execute(
                     """
                         INSERT INTO company_vacancies (company_id, vacancy_name, salary_from, salary_to, alternate_url)
                         VALUES (%s, %s, %s, %s, %s)
                         """,
-                    (company_id, vacancy_name, salary_from, salary_to, alternate_url)
+                    (company_id, vacancy_name, salary_from, salary_to, url_vacancy)
                 )
 
     conn.commit()
