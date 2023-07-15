bb482e569b82a132bec95b5651fd45f784620987
Alina <alinatrofimova649@gmail.com>
2023-07-15 14:13:29 +0800

    list of all companies and number of vacancies

--- src/connecting_database.py
+++ src/connecting_database.py
@@ -4,20 +4,18 @@ import psycopg2
 
 
 class DBManager:
-
-    def __init__(self):
-        pass
-
     def get_companies_and_vacancies_count(self):
+        """получает список всех компаний и количество вакансий у каждой компании"""
         conn = psycopg2.connect(host='localhost', database='coursework_hh_ru', user='postgres', password='123406@aLINA',
                                 port=5432)  # Данные БД
         cur = conn.cursor()  # Включение курсора
         cur.execute("""SELECT title_company, number_vacancies FROM company""")
         rows = cur.fetchall()
-        print(rows)
         for i in rows:
             name_company = i[0]
             count_vacancy = i[1]
-            print(f'')
+            print(f'{name_company}: Количество вакансий - {count_vacancy}')
         cur.close()  # Закрываем курсор
-        conn.close()  # Закр
+        conn.close()  # Закрываем
+
+
